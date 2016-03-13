d484c95f7a951cce6a13b69434634db8a84c600d
Tia Pewitt <tiapewitt@gmail.com>
2016-03-13 17:53:13 -0400

    updates to layout and navbar

--- profile.js
+++ profile.js
@@ -1,3 +1,18 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
+   $(function () {
+        $('#closemodal').click(function () {
+            $('#mymodal').modal('hide');
+        });
+
+    });
+
+    $('body').scrollspy({ target: '#navbar' })
+    $('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+})
+
+    $('.dropdown-toggle').dropdown()
+
+
